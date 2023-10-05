5cf3aebf24529887bb601563f7e079c16cef6696
Dalvin Onyango <dalvin.onyango@students.uonbi.ac.ke>
2023-10-05 12:45:44 -0700

    Printing a statement using puts command in C file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
+/**
+ * 4-puts file 
+ * Author is Dalvin Onyango
+ */
+
 #include <stdio.h>
 /**
- * A c program that prints the line 'Programming is like building a multilingual puzzle' using command puts
+ * A c program that prints "Programming is like building
+ *                         a multilingual puzzle"
+ * using command puts
+ * Return:Always 0
  */
 int main(void)
 {
