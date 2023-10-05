c766681c614e2d19b939688497fdd6ef3fc7eb9a
Dalvin Onyango <dalvin.onyango@students.uonbi.ac.ke>
2023-10-05 12:38:05 -0700

    Printing a statement using puts command in C file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+/**
+ * A c program that prints the line 'Programming is like building a multilingual puzzle' using command puts
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
