77280247df1f3bfc8108ed40e4713a0cbce70a25
Dalvin Onyango <dalvin.onyango@students.uonbi.ac.ke>
2023-10-06 08:06:20 -0700

    Printing  alphabets in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,23 @@
 /*
  * File: 2-print_alphabet.c
- *
  * Author: Dalvin O. Onyango
  */
+
 #include <stdio.h>
 
-/*
- * Main: Program that prints alphabets in
- * 	lowercase,followed by a new line
+/**
+ * main - Prints the alphabet in lowercase.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
 	char ch;
-	for(ch = 'a'; ch <= 'z'; ch++)
+
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
+
 	putchar('\n');
+
 	return (0);
 }
