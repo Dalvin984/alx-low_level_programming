eae3d2dde49b2ccaeb4e3531892ad5d2a947e0f8
Dalvin Onyango <dalvin.onyango@students.uonbi.ac.ke>
2023-10-06 08:17:21 -0700

    Printing alphabets except for q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+/*
+ * File: 4-print_alphabt.c
+ * Author: Dalvin O. Onyango
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
