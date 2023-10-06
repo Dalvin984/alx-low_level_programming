4c6416e05eb72dd06f3598b5861e6bd4e6a8609c
Dalvin Onyango <dalvin.onyango@students.uonbi.ac.ke>
2023-10-06 08:28:39 -0700

    Printing numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+/*
+ * File: 5-print_numbers.c
+ *
+ * Owner: Dalvin O. Onyango
+ */
+#include <stdio.h>
+
+/*
+ * Main: Printing all single digits of base 10
+ * 	starting from 0,followed by new line.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+		
+		printf("%d", i);
+	
+	printf('\n');
+
+	return (0);
+
+}
