88a4228dd9d693320e30b04c48bbc64744d950ac
Dalvin Onyango <dalvin.onyango@students.uonbi.ac.ke>
2023-10-06 05:45:36 -0700

    if loop condition

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,7 +9,8 @@
 #include <stdio.h>
 
 
-/* Main: Determinig if a number is negative,positive or zero
+/* 
+ * Main: Determinig if a number is negative,positive or zero
  *
  * Return : Always 0
  */
@@ -19,11 +20,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n<0)
-		printf("%d is negative\n",n);
-	else if	(n==0)
-		printf("%d is zero\n",n);
+	if (n < 0)
+		printf("%d is negative\n", n);
+	else if	(n == 0)
+		printf("%d is zero\n", n);
 	else
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	return	(0);
 }
