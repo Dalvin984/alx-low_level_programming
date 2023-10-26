53c9d2ecc30395346ab95f3a91164dd476d9c9f0
Dalvin Onyango <dalvin.onyango@students.uonbi.ac.ke>
2023-10-25 18:36:23 -0700

    Returning the lenth of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,25 +1,25 @@
 /*
  * File: 2-strlen_recursion.c
- * Owner: Dalvin O. Onyango
+ * Auth: Dalvin O. Onyango
  */
 
 #include "main.h"
-/*
- * _strlen_recursion - returns the length of a string
- *
- * @s - string to be measured
- *
- * Return:The length of a string
- */
 
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
 int _strlen_recursion(char *s)
-{	
+{
 	int len = 0;
+
 	if (*s)
 	{
 		len++;
 		len += _strlen_recursion(s + 1);
-
 	}
+
 	return (len);
 }
